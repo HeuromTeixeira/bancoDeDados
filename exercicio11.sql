6f40a19773887f655294d30d975d1298571adb47
HeuromTeixeira <heurom.teixeria@mercadolivre.com>
2023-01-23 16:56:30 -0300

    exercicio 01

---
+++ exercicio11.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_funcionario(nome, cpf , salario, setor) 
+UPDATE tb_funcionario SET salario = 1800.00 WHERE id = 4;
+INSERT INTO tb_funcionario(nome , cpf, salario, setor)
+values ("Gustavo",15915948438, 6000.00, "Gerencia");
+'1','Ricardo', '10045678912', '2000', 'Administração'
+'2', 'Paulo', '98748521100', '3000.1', 'Recursos Humanos'
+'3', 'Calos', '85236974125', '5000.22', 'Vendas'
+'4', 'Maria', '98735961400', '1800', 'Cozinha'
+'5', 'Gustavo', '15915948438', '6000', 'Gerencia'
+
